f4ecc4663cbedeb643bb0c2163c7df29e063fbe0
JAngelGC <a01552274@tec.mx>
2022-11-03 23:18:36 -0600

    Second question

--- Agents/Pacman/Pacman_new.py
+++ Agents/Pacman/Pacman_new.py
@@ -5,13 +5,25 @@ from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
 class Ghost(Agent):
+    def __init__(self, model, pos, matrix):
+        super().__init__(model.next_id(), model)
+        self.pos = pos
+        self.matrix = matrix
+
+    def step(self):
+        next_moves = self.model.grid.get_neighborhood(self.pos, moore=False)
+        next_moves_copy = [] # array to save the possible moves
+        for (x,y) in next_moves:
+            if self.matrix[y][x] == 1:
+                next_moves_copy.append((x,y))
+                
+        next_move = self.random.choice(next_moves_copy)
+        self.model.grid.move_agent(self, next_move)
+
+class WallBlock(Agent):
     def __init__(self, model, pos):
         super().__init__(model.next_id(), model)
         self.pos = pos
-    def step(self):
-        next_moves = self.model.grid.get_neighborhood(self.pos, moore=False)
-        next_move = self.random.choice(next_moves)
-        self.model.grid.move_agent(self, next_move)
 
 class Maze(Model):
     def __init__(self):
@@ -19,15 +31,43 @@ class Maze(Model):
         self.schedule = RandomActivation(self)
         self.grid = Grid(17, 14, torus=False)
 
-        ghost = Ghost(self, (8, 6))
+        matrix = [
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0],
+        [0,1,0,1,0,0,0,1,1,1,0,1,0,1,0,1,0],
+        [0,1,1,1,0,1,0,0,0,0,0,1,0,1,1,1,0],
+        [0,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0],
+        [0,1,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0],
+        [0,1,1,1,0,1,0,1,1,1,0,1,0,1,0,1,0],
+        [0,1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,0],
+        [0,1,0,1,1,1,0,0,1,0,0,1,0,1,1,1,0],
+        [0,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0],
+        [0,1,1,1,0,1,0,0,0,0,0,1,0,1,1,1,0],
+        [0,1,0,1,0,1,0,1,1,1,0,0,0,1,0,1,0],
+        [0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        ]
+
+
+        ghost = Ghost(self, (1, 1), matrix)
         self.grid.place_agent(ghost, ghost.pos)
         self.schedule.add(ghost)
+
+        for _,x,y in self.grid.coord_iter():
+            if matrix[y][x] == 0:
+                # for each obstacle, create a WallBlock agent
+                wallB = WallBlock(self, (x,y))
+                self.grid.place_agent(wallB, wallB.pos) # ponerlo en el grid
         
     def step(self):
         self.schedule.step()
 
 def agent_portrayal(agent):
-  return {"Shape": "ghost2.png", "Layer": 0}
+    # render a different object from each agent
+    if isinstance(agent, Ghost):
+        return {"Shape": "ghost2.png", "Layer": 0}
+    if isinstance(agent, WallBlock):
+        return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "Gray", "Layer": 1}
 
 grid = CanvasGrid(agent_portrayal, 17, 14, 450, 450)
 
